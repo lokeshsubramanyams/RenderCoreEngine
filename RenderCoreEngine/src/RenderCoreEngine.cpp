e9454d735e8ed27d90df43d4b3c0c504e928acb7
lokesh <lokeshs26gp@gmail.com>
2023-12-27 17:53:22 +0530

    Initial project setup in VS2022

---
+++ RenderCoreEngine/src/RenderCoreEngine.cpp
@@ -0,0 +1,12 @@
+﻿// RenderCoreEngine.cpp : Defines the entry point for the application.
+//
+
+#include "RenderCoreEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
