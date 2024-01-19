33fb84a03927a6ac87d61cde2beee007ba06fbec
lokesh <lokeshs26gp@gmail.com>
2024-01-19 10:50:47 +0530

    xxd convert to header file in build folder and include it in cpp

--- RenderCoreEngine/libraries/graphicsEngine/openGLEngine/openGL4x/OpenGL4x.cpp
+++ RenderCoreEngine/libraries/graphicsEngine/openGLEngine/openGL4x/OpenGL4x.cpp
@@ -1,5 +1,7 @@
 #include "OpenGL4x.hpp"
 
+#include "default_vert.h"
+
 
 namespace RCEngine
 {
@@ -7,6 +9,22 @@ namespace RCEngine
 	{
 		void OpenGL4x::TestAPIs()
 		{
+
+
+			GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+
+			
+			const GLchar* shaderSource[] = { reinterpret_cast<const GLchar*>(D__2024_Git_Repos_RenderCoreEngine_RenderCoreEngine_libraries_graphicsEngine_openGLEngine_openGL4x_shaders_default_vert) };
+			GLint lengths[] = { static_cast<GLint>(D__2024_Git_Repos_RenderCoreEngine_RenderCoreEngine_libraries_graphicsEngine_openGLEngine_openGL4x_shaders_default_vert_len) };
+
+			// Provide the shader source to OpenGL
+			//glShaderSource(vertShader, 1, shaderSource, lengths);
+
+			// Compile the shader
+			//glCompileShader(vertShader);
+
+
+
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
 
