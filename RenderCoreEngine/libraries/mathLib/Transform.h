ac3744f56de1f53079b013937ccd3df2525d2f32
lokesh <lokeshs26gp@gmail.com>
2024-02-01 21:46:14 +0530

    transformation matrix in transform

--- RenderCoreEngine/libraries/mathLib/Transform.h
+++ RenderCoreEngine/libraries/mathLib/Transform.h
@@ -9,12 +9,19 @@ namespace RCEngine
 		{
 			Vector3 position;
 			Quaternion rotation;
-			Vector3 Scale;
+			Vector3 scale;
 			Transform(Vector3 _pos, Quaternion _rot, Vector3 _scale)
 			{
 				position = _pos;
 				rotation = _rot;
-				Scale = _scale;
+				scale = _scale;
+			}
+			Matrix44 GetTransformationMatrix()
+			{
+				 Matrix44 translation =  glm::translate(Matrix44(1.0f), position);
+				 Matrix44 roation = glm::mat4_cast(rotation);
+				 Matrix44 scaleMat = glm::scale(Matrix44(1.0f), scale);
+				 return translation * roation * scaleMat;
 			}
 		};
 	}
